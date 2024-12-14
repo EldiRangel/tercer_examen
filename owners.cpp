891db4734f5d8b241ce6856833a716e4e5661345
EldiRangel <eldi456.rangel@gmail.com>
2024-12-13 21:18:42 -0800

    librerias y estructura

---
+++ owners.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+struct Owner {
+    string cedula;
+    string nombre;
+    string apellido;
+    string address;
+    string telefono;
+    string filename;
+};
