ae36c511aceb018f3954a4776363820ab695b45d
EldiRangel <eldi456.rangel@gmail.com>
2024-12-13 21:41:32 -0800

    funcion de eliminar

--- owners.cpp
+++ owners.cpp
@@ -49,3 +49,80 @@ void leerOwners(const string &filename) {
         cerr << "Error al abrir el archivo " << filename << "\n";
     }
 }
+
+void actualizarOwner(const string &filename) {
+    string cedula, newCed, newNombre, newApell, newAddress, newPhone;
+    bool exists = false;
+
+    cout << "Ingrese la cedula del owner a actualizar: ";
+    cin >> cedula;
+
+    ifstream file(filename.c_str()); 
+    if (!file.is_open()) {
+        cerr << "Error al abrir el archivo " << filename << "\n";
+        return;
+    }
+
+    string line;
+    while (getline(file, line)) {
+        if (line.substr(0, line.find(',')) == cedula) {
+            exists = true;
+            break;
+        }
+    }
+    file.close();
+
+    if (!exists) {
+        cout << "No se encontro un owner con la cedula: " << cedula << ".\n\n";
+        return;
+    }
+
+    cout << "Status: Modificando\n";
+    cout << "   Ingrese la cedula del owner: ";
+    cin >> newCed;
+    cout << "   Ingrese el nombre del owner: ";
+    cin >> newNombre;
+    cout << "     Ingrese el apellido del owner: ";
+    cin >> newApell;
+    cout << "   Ingrese la direccion del owner: ";
+    cin.ignore();
+    getline(cin, newAddress);
+    cout << "   Ingrese el telefono del owner: ";
+    cin >> newPhone;
+
+    string updatedData = newCed + "," + newNombre + "," + newApell + "," + newAddress + "," + newPhone;
+
+    ifstream inputFile(filename.c_str()); 
+    if (!inputFile.is_open()) {
+        cerr << "Error al abrir el archivo " << filename << "\n";
+        return;
+    }
+
+    ofstream tempFile("temp.csv"); 
+    if (!tempFile.is_open()) {
+        cerr << "Error al abrir el archivo temporal\n";
+        return;
+    }
+
+    bool updated = false;
+
+    while (getline(inputFile, line)) {
+        if (line.substr(0, line.find(',')) == cedula) {
+            tempFile << updatedData << "\n";
+            updated = true;
+        } else {
+            tempFile << line << "\n";
+        }
+    }
+
+    inputFile.close();
+    tempFile.close();
+
+    remove(filename.c_str()); 
+    rename("temp.csv", filename.c_str()); 
+
+    if (updated)
+        cout << "El owner con la cedula: " << cedula << " fue actualizado correctamente.\n\n";
+    else
+        cout << "Hubo un error al actualizar el dueño.\n\n";
+}
