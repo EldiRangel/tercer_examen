cfcccb4c014ebf891b7451a459d46f9e739e5788
EldiRangel <eldi456.rangel@gmail.com>
2024-12-13 20:39:44 -0800

    siguiendo el desarrollo de los voids

--- programa.cpp
+++ programa.cpp
@@ -61,3 +61,40 @@ void actualizarMascota() {
     cout << "Mascota no encontrada.\n";
 }
 
+void eliminarMascota() {
+    string name;
+    cout << "Ingrese el nombre de la mascota a eliminar: ";
+    cin >> name;
+    for (int i = 0; i < petCount; i++) {
+        if (pets[i].name == name) {
+            for (int j = i; j < petCount - 1; j++) {
+                pets[j] = pets[j + 1];
+            }
+            petCount--;
+            cout << "Mascota eliminada.\n";
+            return;
+        }
+    }
+    cout << "Mascota no encontrada.\n";
+}
+
+void agregarOwner() {
+    if (ownerCount < MAX_OWNERS) {
+        cout << "Ingrese el nombre del dueño: ";
+        cin >> owners[ownerCount].name;
+        cout << "Ingrese la dirección: ";
+        cin >> owners[ownerCount].address;
+        cout << "Ingrese el teléfono: ";
+        cin >> owners[ownerCount].phone;
+        ownerCount++;
+        cout << "Dueño agregado exitosamente.\n";
+    } else {
+        cout << "No se pueden agregar más dueños.\n";
+    }
+}
+
+void verOwners() {
+    for (int i = 0; i < ownerCount; i++) {
+        cout << "Nombre: " << owners[i].name << ", Dirección: " << owners[i].address << ", Teléfono: " << owners[i].phone << "\n";
+    }
+}
