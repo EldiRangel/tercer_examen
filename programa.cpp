ecb2307eeeaadde668495eb92f81c5dfdf3b5fb6
EldiRangel <eldi456.rangel@gmail.com>
2024-12-13 20:30:10 -0800

    void para agregar mascotas

--- programa.cpp
+++ programa.cpp
@@ -13,3 +13,27 @@ struct Owner {
     string address;
     string phone;
 };
+
+const int MAX_PETS = 100;
+const int MAX_OWNERS = 100;
+
+Pet pets[MAX_PETS];
+Owner owners[MAX_OWNERS];
+
+int petCount = 0;
+int ownerCount = 0;
+
+void agregarMascota() {
+    if (petCount < MAX_PETS) {
+        cout << "Ingrese el nombre de la mascota: ";
+        cin >> pets[petCount].name;
+        cout << "Ingrese el tipo de mascota: ";
+        cin >> pets[petCount].type;
+        cout << "Ingrese la edad de la mascota: ";
+        cin >> pets[petCount].age;
+        petCount++;
+        cout << "Mascota agregada exitosamente.\n";
+    } else {
+        cout << "No se pueden agregar más mascotas.\n";
+    }
+}
